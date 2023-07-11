0cbf7fb29a3b4a9a2515e0e1fdea1e3b5d170307
Mehrshad <mehrshad.hosseini28@gmail.com>
2023-07-12 01:48:55 +0330

    update setup #0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "clickhouse-query",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Mehrshad Hosseini",
     author_email = "mehrh8@gmail.com",
     description = "clickhouse query",
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir = {"": "src"},
-    packages = setuptools.find_packages(where="src"),
+    package_dir = {"": "clickhouse_query"},
+    packages = setuptools.find_packages(where="clickhouse_query"),
     python_requires = ">=3.6",
 )
