17f2ec8de1010c3cff7123f50a1c4572c9d551a0
Mehrshad <mehrshad.hosseini28@gmail.com>
2024-02-20 16:19:37 +0330

    version 0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="clickhouse-query",
-    version="0.0.5",
+    version="0.0.6",
     author="Mehrshad Hosseini",
     author_email="mehrh8@gmail.com",
     description="clickhouse query",
--- tests/test.py
+++ tests/test.py
@@ -15,3 +15,11 @@ class TestClickhouseQuery(unittest.TestCase):
     def test_field(self):
         q = ch.QuerySet().select(a="b").prewhere(ch.Equals("b", ch.Value("salam")))
         print(ch.get_sql(q))
+
+    def test_multi_filter_on_field(self):
+        q = ch.QuerySet().select(a="b").prewhere(b__lt=4).prewhere(b__lt=5)
+        print(ch.get_sql(q))
+
+    def test_int(self):
+        q = ch.QuerySet().select(a="b").prewhere(ch.Equals("b", 2))
+        print("#1", ch.get_sql(q))
