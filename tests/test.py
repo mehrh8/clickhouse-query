8a46b26fe94b010364e13b94a41dfefcda2d2117
Seyed Mehrshad Hosseini <56117015+mehrh8@users.noreply.github.com>
2024-02-22 15:18:27 +0330

    Refactor QuerySet #12 (#14)

--- tests/test.py
+++ tests/test.py
@@ -23,3 +23,21 @@ class TestClickhouseQuery(unittest.TestCase):
     def test_int(self):
         q = ch.QuerySet().select(a="b").prewhere(ch.Equals("b", 2))
         print("#1", ch.get_sql(q))
+
+    def test_isnull(self):
+        q = ch.QuerySet().select(a="b").prewhere(b__isnull=False)
+        print(ch.get_sql(q))
+
+    def test_clear_where(self):
+        q = ch.QuerySet().select(a="b").where(b=2)
+        q.where(None)
+        print("#3", ch.get_sql(q))
+
+    def test_clear_select(self):
+        q = ch.QuerySet().select(a="b").where(b=2)
+        q.select(None)
+        print("#4", ch.get_sql(q))
+
+    def test_same_filter(self):
+        q = ch.QuerySet().select(a="b").prewhere(b__lt=4).prewhere(b__lt=5)
+        print("#5", ch.get_sql(q))
