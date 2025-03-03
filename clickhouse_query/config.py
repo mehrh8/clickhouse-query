07f1e5954c4ff49df9fae3493dee4573b7961ac7
Mehrshad Hosseini <56117015+mehrh8@users.noreply.github.com>
2025-03-03 09:45:03 +0330

    Move inline condition and inline functions to config file  (#23)
    * Move inline condition and inline functions to config file #0
    
    * fix test

---
+++ clickhouse_query/config.py
@@ -0,0 +1,67 @@
+from clickhouse_query import functions, models
+
+__all__ = ["INLINE_CONDITIONS", "INLINE_FUNCTIONS"]
+
+
+def _exact(field, value):
+    if value is None:
+        return functions.isNull(field)
+    else:
+        return functions.equals(field, value)
+
+
+def _iexact(field, value):
+    if value is None:
+        return functions.isNull(field)
+    else:
+        return functions.iLike(field, value)
+
+
+def _contains(field, value):
+    return functions.like(field, functions.concat(models.Value("%"), value, models.Value("%")))
+
+
+def _icontains(field, value):
+    return functions.iLike(field, functions.concat(models.Value("%"), value, models.Value("%")))
+
+
+def _isnull(field, value):
+    if value.arg is True:
+        return functions.isNull(field)
+    elif value.arg is False:
+        return functions.isNotNull(field)
+    else:
+        raise ValueError("isnull value should be True or False")
+
+
+INLINE_CONDITIONS = {
+    "exact": _exact,
+    "iexact": _iexact,
+    "contains": _contains,
+    "icontains": _icontains,
+    "in": functions.in_,
+    "notin": functions.notIn,
+    "gt": functions.greater,
+    "gte": functions.greaterOrEquals,
+    "lt": functions.less,
+    "lte": functions.lessOrEquals,
+    "startswith": lambda field, value: functions.like(field, functions.concat(value, models.Value("%"))),
+    "istartswith": lambda field, value: functions.iLike(field, functions.concat(value, models.Value("%"))),
+    "endswith": lambda field, value: functions.like(field, functions.concat(models.Value("%"), value)),
+    "iendswith": lambda field, value: functions.iLike(field, functions.concat(models.Value("%"), value)),
+    "isnull": _isnull,
+}
+
+
+INLINE_FUNCTIONS = {
+    "year": functions.toYear,
+    "month": functions.toMonth,
+    "day": functions.toDayOfYear,
+    "week": functions.toWeek,
+    "week_day": functions.toDayOfWeek,
+    "quarter": functions.toQuarter,
+    "time": functions.toTime,
+    "hour": functions.toHour,
+    "minute": functions.toMinute,
+    "second": functions.toSecond,
+}
--- clickhouse_query/utils.py
+++ clickhouse_query/utils.py
@@ -32,88 +32,28 @@ def get_sql(arg, uid_generator: UIDGenerator | None = None):
 
 def _extract_condition(item: str, value: "Value"):
     from clickhouse_query import functions, models
+    from clickhouse_query.config import INLINE_CONDITIONS
 
     *_field, op = item.split("__")
 
     field = models.F(_field)
-    if op == "exact":
-        if value is None:
-            condition = functions.isNull(field)
-        else:
-            condition = functions.equals(field, value)
-    elif op == "iexact":
-        if value is None:
-            condition = functions.isNull(field)
-        else:
-            condition = functions.iLike(field, value)
-    elif op == "contains":
-        condition = functions.like(field, functions.concat(models.Value("%"), value, models.Value("%")))
-    elif op == "icontains":
-        condition = functions.iLike(field, functions.concat(models.Value("%"), value, models.Value("%")))
-    elif op == "in":
-        condition = functions.in_(field, value)
-    elif op == "notin":
-        condition = functions.notIn(field, value)
-    elif op == "gt":
-        condition = functions.greater(field, value)
-    elif op == "gte":
-        condition = functions.greaterOrEquals(field, value)
-    elif op == "lt":
-        condition = functions.less(field, value)
-    elif op == "lte":
-        condition = functions.lessOrEquals(field, value)
-    elif op == "startswith":
-        condition = functions.like(field, functions.concat(value, models.Value("%")))
-    elif op == "istartswith":
-        condition = functions.iLike(field, functions.concat(value, models.Value("%")))
-    elif op == "endswith":
-        condition = functions.like(field, functions.concat(models.Value("%"), value))
-    elif op == "iendswith":
-        condition = functions.iLike(field, functions.concat(models.Value("%"), value))
-    elif op == "isnull":
-        if value.arg is True:
-            condition = functions.isNull(field)
-        elif value.arg is False:
-            condition = functions.isNotNull(field)
-        else:
-            raise ValueError("isnull value should be True or False")
-    else:  # equals
-        field = models.F(_field + [op])
-        condition = functions.equals(field, value)
 
-    return condition
+    if op in INLINE_CONDITIONS:
+        return INLINE_CONDITIONS[op](field, value)
+
+    field = models.F(_field + [op])
+    return functions.equals(field, value)
 
 
 def _apply_operator(field, op):
-    from clickhouse_query import functions
+    from clickhouse_query.config import INLINE_FUNCTIONS
 
-    if op[0] == "_":  # nested
-        field = field._arg_extend("." + op[1:])
-    elif op == "year":
-        field = functions.toYear(field)
-    elif op == "month":
-        field = functions.toMonth(field)
-    elif op == "day":
-        field = functions.toDayOfYear(field)
-    elif op == "week":
-        field = functions.toWeek(field)
-    elif op == "week_day":
-        field = functions.toDayOfWeek(field)
-    elif op == "quarter":
-        field = functions.toQuarter(field)
-    elif op == "time":
-        field = functions.toTime(field)
-    elif op == "hour":
-        field = functions.toHour(field)
-    elif op == "minute":
-        field = functions.toMinute(field)
-    elif op == "second":
-        field = functions.toSecond(field)
-    else:
-        # not changed
-        return field._arg_extend("__" + op)
+    if op[0] == "_":  # nested operator
+        return field._arg_extend("." + op[1:])
+    elif op in INLINE_FUNCTIONS:
+        return INLINE_FUNCTIONS[op](field)
 
-    return field
+    return field._arg_extend("__" + op)
 
 
 def get_expression(v, str_is_field=False):
